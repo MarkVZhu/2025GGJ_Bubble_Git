ab6d071fb8d985790ecc4d64a66eef03b00b563d
Doria078cw <caiweidoria@outlook.com>
2025-01-25 21:25:24 -0500

    UIUpdate2125

---
+++ Assets/Resources/UI/UI Assets/MoveImageUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveImageUp : MonoBehaviour
+{
+    public float speed = 5f;  // 控制移动速度
+
+    void Update()
+    {
+        // 简单地增加y坐标
+        transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+}
